a6766032001eb7bfa796b913545a4f252e4ba265
titanabrianwork <titanabrianwork@gmail.com>
2020-02-25 13:09:08 +0700

    bug fix

--- test/test.js
+++ test/test.js
@@ -5,45 +5,42 @@ const mongoose = require("mongoose");
 const User = require("../models/User");
 const Message = require("../models/Message");
 // Module Test
-
 chai.use(chaiHttp);
-module.exports=()=>{
-    describe("PERFORM UNIT TESTING FOR BACKEND",()=>{
+describe("PERFORM UNIT TESTING FOR BACKEND",()=>{
+    
+    before((done) => {
+        console.log("\t Cleaning database before testing")
+        mongoose.connect(env.parsed.MONGO_TEST_URI,{useNewUrlParser:true,useUnifiedTopology: true})
+        .then()
+        .catch(()=>console.log("Failed to Connect to Database"));
         
-        before((done) => {
-            console.log("\t Cleaning database before testing")
-            mongoose.connect(env.parsed.MONGO_TEST_URI,{useNewUrlParser:true,useUnifiedTopology: true})
-            .then()
-            .catch(()=>console.log("Failed to Connect to Database"));
-            
-            User.deleteMany({}, (err) => { 
-            });   
-            
-            Message.deleteMany({}, (err) => { 
-             });
-             done();  
-        });
-    
-        after((done) => {
-            console.log("\t Cleaning database after testing")
-            mongoose.connect(env.parsed.MONGO_TEST_URI,{useNewUrlParser:true,useUnifiedTopology: true})
-            .then()
-            .catch(()=>console.log("Failed to Connect to Database"));
-            
-            User.deleteMany({}, (err) => { 
+        User.deleteMany({}, (err) => { 
+        });   
+        
+        Message.deleteMany({}, (err) => { 
             });
-    
-            Message.deleteMany({}, (err) => { 
-             }); 
-    
-             done();
+            done();  
+    });
+
+    after((done) => {
+        console.log("\t Cleaning database after testing")
+        mongoose.connect(env.parsed.MONGO_TEST_URI,{useNewUrlParser:true,useUnifiedTopology: true})
+        .then()
+        .catch(()=>console.log("Failed to Connect to Database"));
+        
+        User.deleteMany({}, (err) => { 
         });
-    
-        require("./unauthorized").test
-        require("./register").test;
-        require("./auth").test;
-        require('./message').test;
-    })
-}
+
+        Message.deleteMany({}, (err) => { 
+            }); 
+
+            done();
+    });
+
+    require("./unauthorized").test
+    require("./register").test;
+    require("./auth").test;
+    require('./message').test;
+})
 
 
